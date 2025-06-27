6c123b357115f5ec6db2baf88c89d398fbed9597
HalilAlpay <96889453+HalilAlpay@users.noreply.github.com>
2025-06-27 08:24:40 +0300

    .sd

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,4 +1,4 @@
-.import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import ProductDetails from "../components/ProductDetails/ProductDetails";
 import { useParams } from "react-router-dom";
 
